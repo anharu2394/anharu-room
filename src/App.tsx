814c2daba7209152e81f001b99e12426aa0b18df
Anharu <haruan2394@gmail.com>
2023-02-16 17:15:23 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
+import {  Html, OrbitControls, Sky } from '@react-three/drei';
+import React, { Component, Suspense } from "react";
+import { Physics } from "@react-three/cannon";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box h="100vh">
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Hands />
+          <Physics
+            gravity={[0, -2, 0]}
+            iterations={20}
+            defaultContactMaterial={{
+              friction: 0.09,
+            }}
+          >
+            <Sky />
+            <OrbitControls />
+            <Html transform position={[0, 0, -30]}>
+              <embed
+                type="text/html"
+                src="https://technologies.anharu.me"
+                width="500"
+                height="1000"
+              />
+            </Html>
+            <Html transform position={[20, 0, -30]}>
+              <embed
+                type="text/html"
+                src="https://semp.jp"
+                width="500"
+                height="1000"
+              />
+            </Html>
+            <Html transform position={[-40, 0, -30]}>
+              <embed
+                type="text/html"
+                src="https://pvyoho.weatherdatascience.tokyo"
+                width="2000"
+                height="1400"
+              />
+            </Html>
+            <ambientLight intensity={0.5} />
+          </Physics>
+        </XR>
+      </Canvas>
+    </Box>
   );
 }
 
